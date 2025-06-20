97b0088b0d9090302ba505da484f751ee920bcf9
agungsa11 <agungsatriya660@gmail.com>
2025-06-20 11:32:15 +0700

    update

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,3 +10,6 @@ $routes->get('/about', 'About::index');
 $routes->get('/utama', 'Utama::index');
 $routes->get('/contact', 'Contact::index');
 $routes->get('/portholio-details', 'PortholioDetails::index');
+$routes->set404Override(function() {
+ echo view('errors/not_found');
+});
